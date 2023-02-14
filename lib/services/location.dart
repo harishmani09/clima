dbe004796d8cab417593d6052256a15450ac39a2
harishmani09 <harishmani@gmail.com>
2023-02-14 15:08:46 +0530

    Initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+
+
+class Location{
+  double? latitude;
+  double? longitude;
+
+  Future <void> getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+
+    } catch (e){
+      print(e);
+    }
+
+  }
+
+
+}
+
+
+
+
+void getCurrentLocation() async {
+  LocationPermission permission = await Geolocator.requestPermission();
+  Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+
+}
+
+
+
+
